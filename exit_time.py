ce0962778c34e409cbb276147a0af234d0f02c00
ibayashi-hikaru <hikaru.ccm1023@gmail.com>
2022-01-31 16:16:05 -0800

    update

--- exit_time.py
+++ exit_time.py
@@ -320,7 +320,7 @@ def main():
     config['core'] = 0 
     config['seed'] = -1
     config['num_dim'] = 100
-    config['sharpness_min'] = 6
+    config['sharpness_min'] = 0
     config['sharpness_interval'] = 10
     config['r_min'] = 0.1 
     config['r_interval'] = 0.05
@@ -329,7 +329,7 @@ def main():
     config['batch_size_min'] = 20 
     config['batch_size_interval'] = 80 
     config['exit_trial_num'] = 100 
-    config['interval_sample'] = 20
+    config['interval_sample'] = 100
     config['optim'] = "SGLD"
     #
     config['data'] = 'AVILA2'
