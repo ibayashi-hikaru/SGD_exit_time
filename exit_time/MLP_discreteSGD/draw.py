754cf6fa5943a406fe77c7d4e3843821b851aa3d
ibayashi-hikaru <hikaru.ccm1023@gmail.com>
2022-03-06 15:16:41 -0800

    update

--- exit_time/MLP_discreteSGD/draw.py
+++ exit_time/MLP_discreteSGD/draw.py
@@ -16,7 +16,7 @@ def draw(sharpness_results, lr_results, batch_size_results, r_results):
     coeff_1, _ = stats.pearsonr(x_1, y_1)
     A = np.vstack([x_1, np.ones(len(x_1))]).T
     m_1, c_1 = np.linalg.lstsq(A, y_1, rcond=None)[0]
-    plt.xlabel("$\lambda$:sharpness")
+    plt.xlabel("$\lambda$: sharpness")
     plt.ylabel("$\left(\log(\mathbf{E}[\\nu])\\right)^2$")
     plt.errorbar(x_1, y_1, yerr=log_std+np.sqrt(2)*np.sqrt(y_1)*log_std, fmt='.', capsize=2) 
     plt.plot(x_1, m_1*x_1 + c_1)
@@ -87,7 +87,7 @@ def draw(sharpness_results, lr_results, batch_size_results, r_results):
     coeff_1, _ = stats.pearsonr(x_1, y_1)
     A = np.vstack([x_1, np.ones(len(x_1))]).T
     m_1, c_1 = np.linalg.lstsq(A, y_1, rcond=None)[0]
-    ax1[0].set_xlabel("$\lambda$:sharpness")
+    ax1[0].set_xlabel("$\lambda$: sharpness")
     ax1[0].set_ylabel("$\left(\log(\mathbf{E}[\\nu])\\right)^2$")
     ax1[0].errorbar(x_1, y_1, yerr=log_std+np.sqrt(2)*np.sqrt(y_1)*log_std, fmt='.', capsize=2) 
     ax1[0].plot(x_1, m_1*x_1 + c_1)
