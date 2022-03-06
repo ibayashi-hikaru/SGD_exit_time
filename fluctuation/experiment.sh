b1afe03e71309abee70177674428c99b6efe47d0
ibayashi-hikaru <hikaru.ccm1023@gmail.com>
2022-03-06 12:28:30 -0800

    update

--- fluctuation/experiment.sh
+++ fluctuation/experiment.sh
@@ -1,3 +1,3 @@
-python train.py       --config_fn ./CIFAR2_config.json
-python dump_trails.py --config_fn ./CIFAR2_config.json
+# python train.py       --config_fn ./CIFAR2_config.json
+# python dump_trails.py --config_fn ./CIFAR2_config.json
 python draw.py        --config_fn ./CIFAR2_config.json
