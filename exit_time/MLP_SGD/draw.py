b1cc4213e006c51a675e614134ca3cb30308a74d
ibayashi-hikaru <hikaru.ccm1023@gmail.com>
2022-05-24 18:14:29 -0700

    update

--- exit_time/MLP_SGD/draw.py
+++ exit_time/MLP_SGD/draw.py
@@ -22,7 +22,7 @@ def draw(sharpness_results, lr_results, batch_size_results, r_results):
     A = np.vstack([x_1, np.ones(len(x_1))]).T
     m_1, c_1 = np.linalg.lstsq(A, y_1, rcond=None)[0]
     plt.xlabel("$\lambda_\mathrm{max}$: sharpness")
-    plt.ylabel("$\left(\log(\mathbf{E}[\\nu])\\right)$")
+    plt.ylabel("$\log(\mathbf{E}[\\nu])")
     plt.errorbar(x_1, y_1, yerr=log_std+np.sqrt(2)*np.sqrt(y_1)*log_std, fmt='.', capsize=2) 
     plt.plot(x_1, m_1*x_1 + c_1)
     plt.legend([f'Linear Correlation: {coeff_1:.3g}'])
@@ -89,7 +89,7 @@ def draw(sharpness_results, lr_results, batch_size_results, r_results):
     A = np.vstack([x_1, np.ones(len(x_1))]).T
     m_1, c_1 = np.linalg.lstsq(A, y_1, rcond=None)[0]
     ax1[0].set_xlabel("$\lambda_\mathrm{max}$:sharpness")
-    ax1[0].set_ylabel("$\left(\log(\mathbf{E}[\\nu])\\right)$")
+    ax1[0].set_ylabel("$\log(\mathbf{E}[\\nu])$")
     ax1[0].errorbar(x_1, y_1, yerr=log_std+np.sqrt(2)*np.sqrt(y_1)*log_std, fmt='.', capsize=2) 
     ax1[0].plot(x_1, m_1*x_1 + c_1)
     ax1[0].legend([f'Linear Correlation: {coeff_1:.3g}'])
